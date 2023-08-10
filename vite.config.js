ec727702f6dce792dd85be63f582b72e772350cf
Laura Vera <lauravera@MacBook-Air-de-Laura.local>
2023-08-10 15:02:50 -0500

    fix bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://lauraveraq.github.io/'
+  base:'https://lauraveraq.github.io/',
   plugins: [react()],
 })
